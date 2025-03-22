586b310a6ff52aa7d82c84f74f5003664d357961
Walter Dudnic <walttinhu@hotmail.com>
2025-03-22 20:31:02 +0000

    header e menu toggle

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btnMobile = document.getElementById("btn-mobile");
+  const nav = document.getElementById("nav");
 
+  function toggleMenu() {
+    nav.classList.toggle("active");
+  }
+
+  btnMobile.addEventListener("click", toggleMenu);
+});
